8a6d002fe436d15fb311a9a1cb87b2faedc8e7fc
script2000 <obiri409@gmail.com>
2024-10-13 07:09:28 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/demo_867/",
+    base: "./",
     build: {
         rollupOptions: {
           output: {
