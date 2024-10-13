63d22688db67dfdbca3b78a7fc86435db8d2478c
script2000 <obiri409@gmail.com>
2024-10-13 07:07:49 -0400

    update

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    base: "/demo_867/",
+    build: {
+        rollupOptions: {
+          output: {
+            // Specify the output directory for CSS
+            entryFileNames: '[name].js', // Keep JS files at the root of dist
+            chunkFileNames: '[name].js', // Keep chunk files at the root of dist
+            assetFileNames: (assetInfo) => {
+              if (assetInfo) {
+                // Output CSS files directly to dist
+                return '[name][extname]';
+              }
+              // Default behavior for other asset types
+              return 'assets/[name].[hash][extname]';
+            },
+          },
+        },
+      },
+});
